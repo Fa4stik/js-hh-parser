44d89adb8fdd1198a9cb2df9cb42607fcc0935af
fa4stik <sh.s@slider-ai.ru>
2025-08-01 13:31:00 +0500

    - update port

--- start_api.py
+++ start_api.py
@@ -53,7 +53,7 @@ def main():
             sys.executable, "-m", "uvicorn",
             "qwen:app",
             "--host", "0.0.0.0",
-            "--port", "8000",
+            "--port", "6381",
             "--reload"
         ])
     except KeyboardInterrupt:
